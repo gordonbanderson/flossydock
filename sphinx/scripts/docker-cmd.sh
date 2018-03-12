50d256d1c50e190c17b5fc24dd194a944788971a
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-13 01:01:18 +0700

    WIP: First cut working for simple silverstripe setup

--- sphinx/scripts/docker-cmd.sh
+++ sphinx/scripts/docker-cmd.sh
@@ -2,7 +2,7 @@
 set -e
 
 #default command
-/root/gows/bin/webhook -hooks /root/gows/sphinxhooks.json &
+/root/gows/bin/webhook -hooks /root/gows/sphinxhooks.json -verbose &
 
 #default command
 /usr/local/bin/searchd --nodetach --config /usr/local/etc/sphinx.conf "$@"

e3cd72079112b0a1b8da81556e370580e31a3ac8
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-12 23:58:29 +0700

    WIP (incorrect) webhook now in place

--- sphinx/scripts/docker-cmd.sh
+++ sphinx/scripts/docker-cmd.sh
@@ -2,4 +2,8 @@
 set -e
 
 #default command
+/root/gows/bin/webhook -hooks /root/gows/sphinxhooks.json &
+
+#default command
 /usr/local/bin/searchd --nodetach --config /usr/local/etc/sphinx.conf "$@"
+

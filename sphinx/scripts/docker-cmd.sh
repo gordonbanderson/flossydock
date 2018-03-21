911d73b0806db8f3f17558cdbbc4cb51c2a1ced4
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-21 09:12:12 +0000

    FIX: Remove webhook invocation

--- sphinx/scripts/docker-cmd.sh
+++ sphinx/scripts/docker-cmd.sh
@@ -2,8 +2,5 @@
 set -e
 
 #default command
-/root/gows/bin/webhook -hooks /root/gows/sphinxhooks.json -verbose &
-
-#default command
 /usr/local/bin/searchd --nodetach --config /usr/local/etc/sphinx.conf "$@"
 

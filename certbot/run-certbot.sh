b8893d2870e2c14ac6571601efd99867f37c55a6
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-05-22 21:21:26 +0100

    WIP: hardcoding values for now, taken from manual dry run

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+sudo certbot certonly --standalone --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
 cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem

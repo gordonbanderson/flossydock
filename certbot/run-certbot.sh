a7d05bb7fbb4ff1a5fd695c71a3de7b7ab1bd31f
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-05-22 21:46:44 +0100

    FIX: Remove sudo as Docker runs as root

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo certbot certonly --standalone --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com
+certbot certonly --standalone --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
 cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem

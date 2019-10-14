45a6d01db0bcaea41f39e9022b2db19e861d117c
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-10-14 22:24:26 +0000

    WIP: Certbot now running, not using standalone

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo 'CN:'
+echo $CN
 
-certbot certonly --standalone --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com
+#Do not use standalone here, as that tries to create a new webserver.  Using --webroot and -w means that
+#nginx will do the serving of the auth file
+certbot certonly --debug-challenges -v --no-eff-email --webroot  --staging --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com -w /var/www/public/
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
 cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem

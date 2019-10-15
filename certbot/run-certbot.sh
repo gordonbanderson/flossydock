5db852984e1dce157138c55b0971afc2470d440a
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-10-15 22:52:19 +0000

    WIP: Debug

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -2,7 +2,19 @@
 echo 'CN:'
 echo $CN
 
+
 #Do not use standalone here, as that tries to create a new webserver.  Using --webroot and -w means that
 #nginx will do the serving of the auth file
-certbot certonly -v --no-eff-email --webroot  --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com -w /var/www/public/
+#Use --staging to test creation of certs
+certbot certonly -v --no-eff-email --webroot  --staging --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com -w /var/www/public/
+
+find /  | grep pem
+find / | grep archive
+ls -lh /etc/letsencrypt/live/gordonbanderson.com/
+cat /etc/letsencrypt/live/gordonbanderson.com/privkey.pem
+echo '---- etc le ----'
+ls -lh /etc/letsencrypt
+
+echo '---- etc le archive ----'
+find /etc/letsencrypt/archive/
 

d420c267cc46cdeb5c4d0ac94999d3ba4765f8da
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-10-15 23:20:54 +0000

    MINOR: Remove staging flag

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -6,7 +6,7 @@ echo $CN
 #Do not use standalone here, as that tries to create a new webserver.  Using --webroot and -w means that
 #nginx will do the serving of the auth file
 #Use --staging to test creation of certs
-certbot certonly -v --no-eff-email --webroot  --staging --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com -w /var/www/public/
+certbot certonly -v --no-eff-email --webroot  --preferred-challenges http --email "gordon.b.anderson@gmail.com" --agree-tos -d gordonbanderson.com -d www.gordonbanderson.com -w /var/www/public/
 
 find /  | grep pem
 find / | grep archive
